e0597745e58eee742a1f78b1e9b361a249271042
mostafa <m_poluton@yahoo.com>
2022-10-12 19:04:04 +0300

    Singleton Pattern Added.

---
+++ Singleton/LoadBalancer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton
+{
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
+    public sealed class LoadBalancer
+    {
+        // Note: constructor is 'private'
+        private LoadBalancer()
+        {
+            servers = new List<Server>
+                {
+                  new Server{ Name = "ServerI", IP = "120.14.220.18" },
+                  new Server{ Name = "ServerII", IP = "120.14.220.19" },
+                  new Server{ Name = "ServerIII", IP = "120.14.220.20" },
+                  new Server{ Name = "ServerIV", IP = "120.14.220.21" },
+                  new Server{ Name = "ServerV", IP = "120.14.220.22" },
+                };
+        }
+
+        //Note: System.Lazy<T> available For .NET 4 (or higher) 
+        private static readonly Lazy<LoadBalancer> Lazy = new Lazy<LoadBalancer>(() => new LoadBalancer());
+        public static LoadBalancer Instance { get { return Lazy.Value; } }
+
+
+        private readonly List<Server> servers;
+        private readonly Random random = new Random();
+
+        public Server NextServer
+        {
+            get
+            {
+                int r = random.Next(servers.Count);
+                return servers[r];
+            }
+        }
+    }
+
+    /// <summary>
+    /// Represents the server 
+    /// </summary>
+    public class Server
+    {
+        public string Name { get; set; }
+        public string IP { get; set; }
+    }
+}
+
+//Note: The code above implicitly uses LazyThreadSafetyMode.ExecutionAndPublication as the thread safety mode for the Lazy<LoadBalancer>.
---
+++ Singleton/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj1 = LoadBalancer.Instance;
+            var obj2 = LoadBalancer.Instance;
+            var obj3 = LoadBalancer.Instance;
+            var obj4 = LoadBalancer.Instance;
+
+            // Confirm these are the same instance
+            if (obj1 == obj2 && obj2 == obj3 && obj3 == obj4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Next, load balance 15 requests for a server
+            var balancer = LoadBalancer.Instance;
+            for (int i = 0; i < 15; i++)
+            {
+                string serverName = balancer.NextServer.Name;
+                Console.WriteLine("Dispatch request to: " + serverName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
