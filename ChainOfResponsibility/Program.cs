38669a611635d2967aae0291506905e08dae9ab1
mostafa <m_poluton@yahoo.com>
2022-11-17 10:53:52 +0300

    Chain of Responsibility pattern Added.

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+// Setup Chain of Responsibility
+using ChainOfResponsibility.ConcreteHandlers;
+using ChainOfResponsibility;
+
+Approver director = new Director();
+Approver vicePresident = new VicePresident();
+Approver president = new President();
+
+director.SetSuccessor(vicePresident);
+vicePresident.SetSuccessor(president);
+
+// Generate and process purchase requests
+Purchase p = new Purchase(2034, 350.00, "Supplies");
+director.ProcessRequest(p);
+p = new Purchase(2035, 32590.10, "Project X");
+director.ProcessRequest(p);
+p = new Purchase(2036, 122100.00, "Project Y");
+director.ProcessRequest(p);
+
+
+// Wait for user
+Console.ReadKey();
---
+++ ChainOfResponsibility/Purchase.cs
@@ -0,0 +1,48 @@
+﻿
+namespace ChainOfResponsibility
+{
+    /// <summary>
+    /// Class holding request details
+    /// </summary>
+    public class Purchase
+    {
+        private int number;
+        private double amount;
+        private string purpose;
+
+        // Ctor
+        public Purchase(int number, double amount, string purpose)
+        {
+            this.number = number;
+            this.amount = amount;
+            this.purpose = purpose;
+        }
+
+        /// <summary>
+        /// Gets or sets purchase Number
+        /// </summary>
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets purchase Amount
+        /// </summary>
+        public double Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets purchase Purpose
+        /// </summary>
+        public string Purpose
+        {
+            get { return purpose; }
+            set { purpose = value; }
+        }
+    }
+}
